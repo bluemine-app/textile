dd0d544d0bdd24ba3c03cd543049ae3aea3712f4
Omkar Todkar <omtodkar@gmail.com>
2019-04-10 21:24:18 +0530

    32: add new test document for code block and pre-format syntaxes.

---
+++ test/parse_results.dart
@@ -0,0 +1,36 @@
+/// used for [HeaderSyntax] test.
+var headers = [
+  "Art of Trackers",
+  "Types of Trackers",
+  "Priority levels",
+  "Describing the Issue",
+  "Issue Description",
+  "Component and Assignee"
+];
+
+var codeBlocks = [
+  '''dependencies {
+    implementation 'com.todkars:shimmer-recyclerview:{latest-version}'
+}''',
+  '''&lt;com.todkars.shimmer.ShimmerRecyclerView
+    android:id="@+id/shimmer_recycler_view"
+    android:layout_width="match_parent"
+    android:layout_height="wrap_content"
+    app:shimmer_recycler_colored="true"
+    app:shimmer_recycler_duration="1000" /&gt;''',
+  '''ShimmerRecyclerView a custom RecyclerView library
+Copyright (C) 2019  Omkar Todkar
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see https://www.gnu.org/licenses/gpl.txt'''
+];
